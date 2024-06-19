180fb28b1981f6cd32eab4ddbb72c03991c78564
Bartlomiej Wnuk <bartlomiejwnuk03@gmail.com>
2024-06-19 13:18:04 +0200

    feat: add an endpoint responsible of adding a new crew member to database

--- api/app.py
+++ api/app.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from db_connection import get_all_crew_members
-from fastapi.responses import JSONResponse
-
+from fastapi.responses import JSONResponse 
+from dto.CrewMember import CrewMember 
+import db_connection as db
 
 app = FastAPI()
 
@@ -12,3 +13,27 @@ async def root():
 @app.get("/crew", response_class=JSONResponse)
 async def get_crew():
     return get_all_crew_members()
+
+@app.post("/crew", response_class=JSONResponse, status_code=status.HTTP_201_CREATED)
+async def post_crew_member(member: CrewMember):
+    new_member = db.create_crew_member(member)
+    if new_member is not None and new_member.passenger_id == member.passenger_id:
+        return {"message": f"Object with ID {member.passenger_id} created successfully."}
+    else: 
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Member of ID {member.passenger_id} already exists."
+        )
+    
+@app.get("/crew/{passenger_id}")
+async def get_crew_member(passenger_id: str):
+    pass
+
+@app.delete("/crew/{passenger_id}", response_class=JSONResponse)
+async def delete_crew_member(passenger_id: str):
+    pass
+
+
+
+
+
--- api/db_connection.py
+++ api/db_connection.py
@@ -1,13 +1,14 @@
 from typing import List
+import dto.CrewMember
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
-from models.CrewMember import CrewMember
+from orm.CrewMember import CrewMember as CrewMemberORM
 
 engine = create_engine(f"postgresql://postgres:1234@localhost:6666/titanic")  
 session = Session(engine)
 
 
-def get_all_crew_members() -> List[CrewMember]: 
+def get_all_crew_members() -> List[CrewMemberORM]: 
     # member = CrewMember
     # statement = select(
     #     member.passenger_name,
@@ -16,7 +17,26 @@ def get_all_crew_members() -> List[CrewMember]:
     #     member.embarked
     #     )
     # return session.scalars(statement).all()
-    return session.query(CrewMember).all()
+    return session.query(CrewMemberORM).all()
 
+
+def create_crew_member(member: dto.CrewMember.CrewMember) -> CrewMemberORM:
+    member_orm = CrewMemberORM(**member.model_dump())
+    
+    try:
+        session.add(member_orm)
+        session.commit()
+        new_member = (
+            session
+            .query(CrewMemberORM)
+            .where(CrewMemberORM.passenger_id.in_([member.passenger_id]))
+            .first()
+            )
+        
+        return new_member
+        
+    except Exception as e:
+        print(e)
+        return None
 
 
